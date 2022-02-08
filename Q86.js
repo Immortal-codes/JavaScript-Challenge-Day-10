f2da37c407a7ea6a43bff277e4108aefd1c90070
Immortal-codes <rajanraj8979@gmail.com>
2022-02-08 17:43:15 +0530

    first commit

---
+++ Q86.js
@@ -0,0 +1,20 @@
+//Write a JavaScript program to find the types of a given angle.
+
+function angle_Type(angle) {
+    if(angle < 90) {
+      return "Acute angle.";
+    }
+    if(angle === 90) {
+      return "Right angle.";
+    }
+    if(angle < 180) {
+      return "Obtuse angle.";
+    }
+    return "Straight angle.";
+  }
+  
+  console.log(angle_Type(47))
+  console.log(angle_Type(90))
+  console.log(angle_Type(145))
+  console.log(angle_Type(180))
+  
